d4020df721d5cb3f6f9e0884c23690b11c0dc204
linh <danglinhloveu@gmail.com>
2026-05-26 23:45:53 +0700

    ver

--- beSCRL/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ beSCRL/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2120ffd6377fae97df9fd1aa3ab0901f069a2a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251f35f75b0b705a5870f3f29f6148936d1f997f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- beSCRL/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ beSCRL/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2120ffd6377fae97df9fd1aa3ab0901f069a2a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251f35f75b0b705a5870f3f29f6148936d1f997f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
